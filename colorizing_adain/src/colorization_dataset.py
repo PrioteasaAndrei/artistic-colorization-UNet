f8cb04e61b0c471d9aa907331010fb09bf0e1827
Matteo-Malve <matteo.malvestiti@outlook.com>
2024-03-14 15:06:06 +0100

    add some files

---
+++ colorizing_adain/src/colorization_dataset.py
@@ -0,0 +1,118 @@
+import torch
+import torchvision
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from zipfile import ZipFile
+from PIL import Image
+from skimage import color
+from datasets import load_dataset
+from dotenv import load_dotenv
+import os
+from tqdm import tqdm
+from torch.utils.data import Dataset, DataLoader
+
+torch.manual_seed(1)
+load_dotenv()
+
+# Define a custom transformation to convert RGB to LAB color space
+class RGBtoLAB(object):
+    def __call__(self, img):
+        # Convert the image from RGB to LAB color space
+        ## TODO: normalize
+        lab_img = color.rgb2lab(img.permute(1, 2, 0).cpu().numpy()).transpose(2, 0, 1) 
+        #lab_img = lab_img / 100  # Scale L channel to [0, 1]
+        lab_img[0, :, :] = lab_img[0, :, :] / 100  # Scale L channel to [0, 1]
+        lab_img[1:, :, :] = (lab_img[1:, :, :] + 128) / 255  # Scale a and b channels to [0, 1]
+        return torch.tensor(lab_img) 
+     
+# Define a custom transformation to convert LAB to grayscale
+class LABtoGray(object):
+    def __call__(self, lab_img):
+        # Convert LAB image to grayscale
+        gray_img = lab_img[0:1, :, :]  # Extract the L channel (1st channel)
+        return torch.tensor(gray_img)
+
+transform_train = torchvision.transforms.Compose([
+    torchvision.transforms.Resize((256, 256)),
+    torchvision.transforms.ToTensor(),
+    RGBtoLAB()
+])
+
+transform_test = torchvision.transforms.Compose([
+    torchvision.transforms.Resize((256, 256)),
+    torchvision.transforms.ToTensor(),
+    RGBtoLAB(),
+    #LABtoGray(),
+])
+
+# Define a custom dataset class for automatic batching
+class ColorizationDataset(Dataset):
+    def __init__(self, data, test=False):
+        self.data = data
+        self.test = test
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        image = self.data[idx]['image']
+        if self.test:
+            return {'image': image}
+        else:
+            gray_scale = self.data[idx]['grayscale_image']
+            return {'image': image, 'grayscale_image': gray_scale}
+
+
+def check_range(tensor):
+    return (tensor.min() >= 0) and (tensor.max() <= 1)
+
+
+def prepare_dataset(train_size=10,test_size=10,batch_size=4):
+    '''
+    check out this for docs: https://huggingface.co/docs/datasets/stream
+
+    you need to make list(output)
+    the returned shapes of the images should be:
+    train: [3,256,256]
+    test: [1,256,256]
+
+    returns:
+    - colorization_dataloader_train: DataLoader
+    - colorization_dataloader_test: DataLoader
+    '''
+    login_token = os.getenv('HUGGING_FACE_TOKEN')
+    dataset_train = load_dataset("imagenet-1k",split='train',use_auth_token=login_token,streaming=True)
+    dataset_test = load_dataset("imagenet-1k",split='test',use_auth_token=login_token,streaming=True)
+    ## map resize transformation before take 
+    transformed_train = dataset_train.map(lambda x: {'image': transform_train(x['image']), 'grayscale_image': transform_test(x['image']),'label': torch.tensor(x['label'])})
+    transformed_test = dataset_test.map(lambda x: {'image': transform_test(x['image']),'label': torch.tensor(x['label'])})
+    
+    ## shuffle train dataset
+    # transformed_train = transformed_train.shuffle()
+
+    ## normalization check
+    normalization_check(list(transformed_train.take(2)), list(transformed_test.take(2)))
+ 
+    print("Dataset loaded successfully")
+    return transformed_train.take(train_size),transformed_test.take(test_size)
+    
+
+def prepare_dataloader(train_data,test_data,batch_size=4):
+    ## prepare data loader
+    colorization_dataset_train = ColorizationDataset(list(train_data), test=False)
+    colorization_dataloader_train = DataLoader(colorization_dataset_train, batch_size=batch_size, shuffle=True, num_workers=4)
+
+    colorization_dataset_test = ColorizationDataset(list(test_data), test=True)
+    colorization_dataloader_test = DataLoader(colorization_dataset_test, batch_size=batch_size, shuffle=False, num_workers=4)
+    
+    print("Data loader prepared successfully")
+    return colorization_dataloader_train, colorization_dataloader_test
+
+
+def normalization_check(transformed_train_list, transformed_test_list):
+  
+    assert check_range(transformed_train_list[0]['image']) == True
+    assert check_range(transformed_test_list[0]['image']) == True
+
+    display('All tests passed')
---
+++ colorizing_adain/src/train_and_test.py
@@ -0,0 +1,169 @@
+import argparse
+from pathlib import Path
+import torch
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torch.utils.data as data
+from PIL import Image, ImageFile
+from tensorboardX import SummaryWriter
+from torchvision import transforms
+from tqdm import tqdm
+from IPython.display import Markdown, display
+from net import VGG_Encoder, Decoder, Net
+# from sampler import InfiniteSamplerWrapper
+import yaml
+import matplotlib.pyplot as plt
+from colorization_dataset import prepare_dataset, prepare_dataloader
+
+cudnn.benchmark = True
+Image.MAX_IMAGE_PIXELS = None  # Disable DecompressionBombError
+# Disable OSError: image file is truncated
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+
+def train_transform():
+    transform_list = [
+        transforms.Resize(size=(512, 512)),
+        transforms.RandomCrop(256),
+        transforms.ToTensor()
+    ]
+    return transforms.Compose(transform_list)
+
+
+class FlatFolderDataset(data.Dataset):
+    def __init__(self, root, transform):
+        super(FlatFolderDataset, self).__init__()
+        self.root = root
+        self.paths = list(Path(self.root).glob('*'))
+        self.transform = transform
+
+    def __getitem__(self, index):
+        path = self.paths[index]
+        img = Image.open(str(path)).convert('RGB')
+        img = self.transform(img)
+        return img
+
+    def __len__(self):
+        return len(self.paths)
+
+    def name(self):
+        return 'FlatFolderDataset'
+    
+
+
+def adjust_learning_rate(cfg, optimizer, iteration_count):
+    """Imitating the original implementation"""
+    lr = float(cfg["lr"]) / (1.0 + float(cfg["lr_decay"]) * iteration_count)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+
+def train(net,cfg):
+
+    # Setup directories for saving models and logs
+    save_dir = Path(cfg['save_dir'])
+    log_dir = Path(cfg['log_dir'])
+
+    # Setup device: MPS for Mac, CUDA for Linux, Windows or Colab
+    device = torch.device("cuda" if torch.cuda.is_available()
+                else "mps" if torch.backends.mps.is_built() else "cpu")
+    display(Markdown(f'Using device: {device}'))
+    
+    # Setup tensorboard writer
+    writer = SummaryWriter(log_dir=str(cfg['log_dir']))
+
+    # Initialize network
+    encoder = VGG_Encoder()
+    decoder = Decoder()
+    network = Net(encoder, decoder)
+    network.train()
+    network.to(device)
+
+    # Setup train dataset and dataloader
+    train_data, test_data = prepare_dataset(train_size=cfg['train_size'], test_size=cfg['test_size'])
+    train_loader, _ = prepare_dataloader(train_data, test_data, batch_size=cfg['batch_size'])
+
+    # Setup optimizer
+    optimizer = torch.optim.Adam(network.decoder.parameters(), lr=float(cfg["lr"]))
+
+    # For every epoch
+    for epoch in tqdm(range(cfg["max_iter"])):
+        running_loss = 0.
+        last_loss = 0.
+        adjust_learning_rate(cfg, optimizer, iteration_count=epoch)
+
+        # For every batch
+        for i, batch_data in enumerate(train_loader):
+            # Unpack colour and greyscale images
+            colour_images=batch_data['image']
+            greyscale_images=batch_data['grayscale_image']
+            print(f"colour_images: {colour_images.shape}")
+            # Move images to device
+            colour_images=colour_images.to(device)
+            greyscale_images=greyscale_images.to(device)
+
+            # Zero your gradients for every batch!
+            optimizer.zero_grad()
+
+            # Forward pass: compiue loss and its gradients
+            reproduced_image,loss=network([colour_images,greyscale_images])
+            loss.backward()
+
+            # Adjust learning weights
+            optimizer.step()
+
+            # Gather data and report
+            running_loss += loss.item()
+
+            if i % 10 == 9:
+                last_loss = running_loss / 10 # loss per batch
+                print('  batch {} loss: {}'.format(i + 1, last_loss))
+                display(Markdown(f'reproduced_image min: {reproduced_image.min()} max: {reproduced_image.max()}') )
+                running_loss = 0.
+
+        writer.add_scalar('loss', loss.item(), epoch + 1)
+        """ writer.add_scalar('loss_content', loss_c.item(), i + 1)
+        writer.add_scalar('loss_style', loss_s.item(), i + 1) """
+
+        if (epoch + 1) % cfg["save_model_interval"] == 0 or (epoch + 1) == cfg["max_iter"]:
+            state_dict = net.decoder.state_dict()
+            for key in state_dict.keys():
+                state_dict[key] = state_dict[key].to(torch.device('cpu'))
+            torch.save(state_dict, save_dir /
+                    'decoder_iter_{:d}.pth.tar'.format(epoch + 1))
+    writer.close()
+
+
+def test_model(net, cfg):
+    # Set the model to evaluation mode
+    net.eval()
+
+    # Prepare the test data loader
+    train_data, test_data = prepare_dataset(train_size=cfg['train_size'], test_size=cfg['test_size'])
+    _, test_loader = prepare_dataloader(train_data, test_data, batch_size=3)
+
+    # Iterate over the data loader
+    for batch in test_loader:
+        original_images = batch['image']
+        
+        # Forward pass through the network
+        with torch.no_grad():
+            reproduced_images = net([original_images])
+            for reproduced_image in reproduced_images:
+                display(Markdown(f'reproduced_image min: {reproduced_image.min()} max: {reproduced_image.max()}') )
+
+        # Convert tensors to numpy arrays and transpose to correct format for visualization
+        original_images_np = original_images.permute(0, 2, 3, 1).cpu().numpy()
+        reproduced_images_np = reproduced_images.permute(0, 2, 3, 1).cpu().numpy()
+
+        # Display original and recreated images
+        for i in range(original_images.size(0)):
+            fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+            axes[0].imshow(original_images_np[i])
+            axes[0].set_title('Original Image')
+            axes[0].axis('off')
+            axes[1].imshow(reproduced_images_np[i])
+            axes[1].set_title('Recreated Image')
+            axes[1].axis('off')
+            plt.show()
