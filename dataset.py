b6cd8039a0c56beae6af6f4b6b5db18b03b6341d
prio <cristi.prioteasa@stud.acs.upb.ro>
2024-03-15 15:05:02 +0100

    debugging

--- dataset.py
+++ dataset.py
@@ -20,14 +20,35 @@ class RGBtoLAB(object):
     def __call__(self, img):
         # Convert the image from RGB to LAB color space
         ## one image is grayscale just repeat channels
+        ## or shape[1] because it is batched?
         if img.shape[0] == 1:
             img = img.repeat(3,1,1)
 
+        ## color.rgb2lab expects shape {H, W, C}
+        ## img is shape {C, H, W}
+        ## lab_img is shape {C, H, W}
         lab_img = color.rgb2lab(img.permute(1, 2, 0).cpu().numpy()).transpose(2, 0, 1) 
-        lab_img = lab_img / 100  # Scale L channel to [0, 1]
-        lab_img[1:, :, :] = (lab_img[1:, :, :] + 128) / 255  # Scale a and b channels to [0, 1]
+        lab_img[0,:,:] = lab_img[0,:,:] / 100  # Scale L channel to [0, 1] | this is from 0 to 100
+        lab_img[1:, :, :] = (lab_img[1:, :, :] + 128) / 255  # Scale a and b channels to [0, 1] | are in -128 127 range
+        
         return torch.tensor(lab_img) 
      
+class LABtoRGB(object):
+    def __call__(self, lab_img):
+        # Scale the LAB channels back to their original ranges
+        lab_img[0, :, :] = lab_img[0, :, :] * 100  # Scale L channel back to [0, 100]
+        lab_img[1:, :, :] = lab_img[1:, :, :] * 255 - 128  # Scale a and b channels back to [-128, 127]
+        
+        # Convert LAB image to RGB color space
+        ## TODO: not sure if transpose is necessary
+        lab_img = lab_img.permute(1, 2, 0)
+        rgb_img = color.lab2rgb(lab_img.cpu().numpy())
+
+        # Convert the resulting RGB image to unnormalized format (0-255)
+        rgb_img_unnormalized = (rgb_img * 255).astype(np.uint8)
+
+        return torch.tensor(rgb_img_unnormalized).permute(2, 0, 1)
+
 # Define a custom transformation to convert LAB to grayscale
 class LABtoGray(object):
     def __call__(self, lab_img):
@@ -50,18 +71,21 @@ transform_test = torchvision.transforms.Compose([
 
 # Define a custom dataset class for automatic batching
 class ColorizationDataset(Dataset):
-    def __init__(self, data):
+    def __init__(self, data, test=False):
         self.data = data
-        
+        self.test = test
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
         image = self.data[idx]['image']
-        gray_scale = self.data[idx]['grayscale_image']
-      
-        return {'image': image, 'grayscale_image': gray_scale}
+        if self.test:
+            return {'image': image}
+        else:
+            gray_scale = self.data[idx]['grayscale_image']
+            return {'image': image, 'grayscale_image': gray_scale}
+
 
 
 def check_range(tensor):
@@ -85,8 +109,6 @@ def prepare_dataset(train_size=10,test_size=10,batch_size=4):
     dataset_train = load_dataset("imagenet-1k",split='train',use_auth_token=login_token,streaming=True)
     dataset_test = load_dataset("imagenet-1k",split='test',use_auth_token=login_token,streaming=True)
     ## map resize transformation before take 
-    ## TODO: 25th image is grayscale and fucks up the dataloader
-    ## its because of the color transformation from skimage that doesnt work so overwrite that
     transformed_train = dataset_train.map(lambda x: {'image': transform_train(x['image']), 'grayscale_image': transform_test(x['image']),'label': torch.tensor(x['label'])})
     transformed_test = dataset_test.map(lambda x: {'image': transform_test(x['image']),'label': torch.tensor(x['label'])})
     
@@ -101,6 +123,13 @@ def prepare_dataset(train_size=10,test_size=10,batch_size=4):
     
 
 def prepare_dataloader(train_data,test_data,batch_size=4):
+    '''
+    function description
+
+    returns:
+    - colorization_dataloader_train: DataLoader
+    - colorization_dataloader_test: DataLoader
+    '''
     ## prepare data loader
 
     list_train_data = list(train_data)
@@ -110,7 +139,7 @@ def prepare_dataloader(train_data,test_data,batch_size=4):
     colorization_dataset_train = ColorizationDataset(train_data)
     colorization_dataloader_train = DataLoader(colorization_dataset_train, batch_size=batch_size, shuffle=True, num_workers=4)
 
-    colorization_dataset_test = ColorizationDataset(list(test_data))
+    colorization_dataset_test = ColorizationDataset(list(test_data), test=True)
     colorization_dataloader_test = DataLoader(colorization_dataset_test, batch_size=batch_size, shuffle=False, num_workers=4)
     
     print("Data loader prepared successfully")
